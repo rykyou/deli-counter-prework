c2b176816815962ff9099a6eea85b2aae0a3361e
Robin Kim <rkim1214@gmail.com>
2018-10-17 02:29:30 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,4 +1,4 @@
-def line (queue_list)
+def line(queue_list)
   if queue_list.size > 0
     a = "The line is currently:"
     queue_list.each_with_index { |name, index| 
@@ -11,10 +11,10 @@ def line (queue_list)
   end 
 end 
 
-def take_a_number (queue_list, new_customer)
+def take_a_number(queue_list, new_customer)
   queue_list.push(new_customer)
   puts "Welcome, #{new_customer}. You are number #{queue_list.size} in line."
 end 
 
-def now_serving
+def now_serving(queue_list)
 end 
